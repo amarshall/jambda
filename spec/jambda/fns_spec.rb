1df315546a6132b420aba8e425a90d87ee354ab8
Andrew Marshall <andrew@johnandrewmarshall.com>
2015-08-01 11:43:37 -0400

    Add test for cons fn

--- spec/jambda/fns_spec.rb
+++ spec/jambda/fns_spec.rb
@@ -4,6 +4,11 @@ require 'jambda/repl'
 describe "core/stdlib fns" do
   refer Jambda::REPL, :rep
 
+  specify "cons" do
+    input = '(cons 4 (list 1 2 3))'
+    expect(rep(input)).to eq '(4 1 2 3)'
+  end
+
   specify "reduce" do
     input = '(reduce + 0 (list 1 2 3 4))'
     expect(rep(input)).to eq '10'
