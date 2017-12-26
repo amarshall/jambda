05ab31647eae093d1422d661c96d572ac36b6806
Andrew Marshall <andrew@johnandrewmarshall.com>
2017-12-25 20:44:40 -0500

    rust: Basic tokenizing lexer
    Replace nom with simple regex

--- rust/src/jambda/reader/lexer.rs
+++ rust/src/jambda/reader/lexer.rs
@@ -1,229 +1,125 @@
-use nom;
-use nom::{digit, rest_s};
-use std;
-use std::str::FromStr;
+use regex::Regex;
 
-#[derive(Debug, PartialEq)]
-pub enum Token {
-  Comment(String),
-  Identifier(String),
-  Integer(isize),
-  LParen,
-  RParen,
-  String(std::string::String),
-}
+const REGEX: &str = r###"(?x)(
+\s+ # Whitespace
+|
+[\\,;"()] # Separator
+|
+[^\s\\,;"()]+ # Word
+)"###;
 
-named!(
-  parse_all<&str, Vec<Token>>,
-  ws!(many0!(alt!(
-    map!(comment, Token::Comment) |
-    map!(integer_literal, Token::Integer) |
-    map!(string_literal, Token::String) |
-    value!(Token::LParen, tag_s!("(")) |
-    value!(Token::RParen, tag_s!(")")) |
-    map!(identifier, Token::Identifier)
-    )))
-);
-
-named!(comment<&str, String>,
-  map!(
-    recognize!(
-      tuple!(
-        tag_s!(";"),
-        alt!(
-          take_until_and_consume_s!("\n") |
-          rest_s
-        )
-      )
-    ),
-    String::from
-  )
-);
-
-named!(integer_literal<&str, isize>, map_res!(digit, isize::from_str));
-
-named!(identifier<&str, String>,
-  map!(
-    recognize!(
-      preceded!(not!(digit), is_not_s!(" \t\r\n"))
-    ),
-    String::from
-  )
-);
-
-named!(string_literal<&str, String>,
-  map!(
-    delimited!(
-      tag_s!("\""),
-      take_until_s!("\""),
-      tag_s!("\"")
-    ),
-    String::from
-  )
-);
-
-pub fn parse(str: &str) -> Vec<Token> {
-  match parse_all(str) {
-    nom::IResult::Done(rest, parsed) => {
-      if rest.is_empty() {
-        parsed
-      } else {
-        panic!("Parse error, remaining tokens: {:?}", rest)
-      }
-    }
-    nom::IResult::Error(err) => {
-      panic!("Parse error: {:?}", err)
-    }
-    nom::IResult::Incomplete(needed) => {
-      panic!("Parse error, incomplete, needed: {:?}", needed)
-    }
+pub fn tokenize(str: &str) -> Vec<String> {
+  let mut tokens = vec![];
+  let re = Regex::new(REGEX).unwrap();
+  for captures in re.captures_iter(&str) {
+    tokens.push((&captures[0]).to_string());
   }
+  tokens
 }
 
 #[cfg(test)]
 mod tests {
   use super::*;
-  use nom::IResult::Done;
 
   #[test]
-  fn test_integer_literal() {
-    assert_eq!(integer_literal("42"), Done("", 42))
-  }
-
-  #[test]
-  fn test_identifier() {
+  fn test_single_token() {
     let input = "abc1";
-    assert_eq!(parse(input), [Token::Identifier("abc1".to_string())])
+    assert_eq!(tokenize(input), ["abc1"])
   }
 
   #[test]
-  fn test_identifier_unicode() {
-    let input = "áḃç1";
-    assert_eq!(parse(input), [Token::Identifier("áḃç1".to_string())])
+  fn test_multiple_tokens() {
+    let input = "abc1 123a";
+    assert_eq!(tokenize(input), ["abc1", " ", "123a"])
   }
 
   #[test]
-  fn test_multiple() {
-    let input = "abc1 123";
-    assert_eq!(parse(input), [Token::Identifier("abc1".to_string()), Token::Integer(123)])
+  fn test_unicode_words() {
+    let input = "áβc1";
+    assert_eq!(tokenize(input), ["áβc1"])
   }
 
   #[test]
-  fn test_whitespace() {
-    let input = "  abc1  123  ";
-    assert_eq!(parse(input), [Token::Identifier("abc1".to_string()), Token::Integer(123)])
+  fn test_multiple_whitespace() {
+    let input = "  abc1  123a  ";
+    assert_eq!(tokenize(input), ["  ", "abc1", "  ", "123a", "  "])
   }
 
   #[test]
   fn test_a_string() {
     let input = r#""foo bar""#;
-    assert_eq!(parse(input), [Token::String("foo bar".to_string())])
+    assert_eq!(tokenize(input), ["\"", "foo", " ", "bar", "\""])
   }
 
   #[test]
-  fn test_left_paren() {
-    let input = "(";
-    assert_eq!(parse(input), [Token::LParen])
+  fn test_escaped_double_quote() {
+    let input = r#"foo\"bar"#;
+    assert_eq!(tokenize(input), ["foo", r"\", r#"""#, "bar"])
   }
 
   #[test]
-  fn test_right_paren() {
-    let input = ")";
-    assert_eq!(parse(input), [Token::RParen])
+  fn test_escaped_backslash_then_double_quote() {
+    let input = r#""foo\\\""#;
+    assert_eq!(tokenize(input), [r#"""#, "foo", r"\", r"\", r"\", r#"""#])
+  }
+
+  #[test]
+  fn test_empty_list() {
+    let input = "()";
+    assert_eq!(tokenize(input), ["(", ")"])
   }
 
   #[test]
   fn test_single_list() {
     let input = "(1 2 3)";
-    assert_eq!(parse(input), [
-      Token::LParen,
-      Token::Integer(1),
-      Token::Integer(2),
-      Token::Integer(3),
-      Token::RParen,
-    ])
+    assert_eq!(tokenize(input), ["(", "1", " ", "2", " ", "3", ")"])
   }
 
   #[test]
   fn test_nested_list() {
     let input = "(1 (2) 3)";
-    assert_eq!(parse(input), [
-      Token::LParen,
-      Token::Integer(1),
-      Token::LParen,
-      Token::Integer(2),
-      Token::RParen,
-      Token::Integer(3),
-      Token::RParen,
-    ])
+    assert_eq!(tokenize(input), ["(", "1", " ", "(", "2", ")", " ", "3", ")"])
   }
 
   #[test]
   fn test_nested_list_empty() {
     let input = "((((()))))";
-    assert_eq!(parse(input), [
-      Token::LParen,
-      Token::LParen,
-      Token::LParen,
-      Token::LParen,
-      Token::LParen,
-      Token::RParen,
-      Token::RParen,
-      Token::RParen,
-      Token::RParen,
-      Token::RParen,
-    ])
+    assert_eq!(tokenize(input), ["(", "(", "(", "(", "(", ")", ")", ")", ")", ")"])
   }
 
   #[test]
   fn test_nested_list_consecutive_parens() {
     let input = "(((1 2 3)))";
-    assert_eq!(parse(input), [
-      Token::LParen,
-      Token::LParen,
-      Token::LParen,
-      Token::Integer(1),
-      Token::Integer(2),
-      Token::Integer(3),
-      Token::RParen,
-      Token::RParen,
-      Token::RParen,
-    ])
+    assert_eq!(tokenize(input), ["(", "(", "(", "1", " ", "2", " ", "3", ")", ")", ")"])
   }
 
   #[test]
   fn test_comments_no_prior_content() {
     let input = "; comment";
-    assert_eq!(parse(input), [Token::Comment("; comment".to_string())])
+    assert_eq!(tokenize(input), [";", " ", "comment"])
   }
 
   #[test]
   fn test_comments_prior_content() {
     let input = "42; comment";
-    assert_eq!(parse(input), [Token::Integer(42), Token::Comment("; comment".to_string())])
+    assert_eq!(tokenize(input), ["42", ";", " ", "comment"])
   }
 
   #[test]
   fn test_comments_content_after() {
     let input = "42 ; comment\n 43";
-    assert_eq!(parse(input), [Token::Integer(42), Token::Comment("; comment\n".to_string()), Token::Integer(43)])
+    assert_eq!(tokenize(input), ["42", " ", ";", " ", "comment", "\n ", "43"])
   }
 
   #[test]
   fn test_consecutive_symbol_chars() {
-    let input = "= > < * /";
-    assert_eq!(parse(input), [
-      Token::Identifier("=".to_string()),
-      Token::Identifier(">".to_string()),
-      Token::Identifier("<".to_string()),
-      Token::Identifier("*".to_string()),
-      Token::Identifier("/".to_string()),
-    ])
+    let input = "=><*/";
+    assert_eq!(tokenize(input), ["=><*/"])
   }
 
   #[test]
   fn test_symbol_with_word_boundaries() {
     let input = "foo!-bar?*";
-    assert_eq!(parse(input), [Token::Identifier("foo!-bar?*".to_string())])
+    assert_eq!(tokenize(input), ["foo!-bar?*"])
   }
 }
