4b502970560344d74a51ab945a45c2dc263e7c0e
Andrew Marshall <andrew@johnandrewmarshall.com>
2018-01-03 01:42:06 -0500

    rust: Test stack depth

--- rust/src/jambda/reader/mod.rs
+++ rust/src/jambda/reader/mod.rs
@@ -4,3 +4,17 @@ pub mod parser;
 pub fn read(str: String) -> Result<parser::Form, String> {
   parser::parse_all(lexer::tokenize(str.as_str()))
 }
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  #[ignore]
+  fn test_read_depth() {
+    let depth = 1_900;
+    let mut input = "(".repeat(depth);
+    input.push_str(")".repeat(depth).as_str());
+    assert!(read(input).is_ok());
+  }
+}
