effdf2019488277f861f0bcb72041b18d09276d7
Andrew Marshall <andrew@johnandrewmarshall.com>
2018-01-03 20:40:55 -0500

    rust: Print Nothing as empty string

--- rust/src/jambda/printer.rs
+++ rust/src/jambda/printer.rs
@@ -1,5 +1,8 @@
 use jambda::reader::parser::Form;
 
 pub fn print(form: Result<Form, String>) -> Result<String, String> {
-  form.map(|f| format!("{:?}", f))
+  form.map(|form2| match form2 {
+    Form::Nothing => "".to_string(),
+    _ => format!("{:?}", form2),
+  })
 }
