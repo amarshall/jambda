eb04bb69dc2c4d90e5f56f4a08f28911382e1eb5
Andrew Marshall <andrew@johnandrewmarshall.com>
2018-05-23 02:15:39 -0400

    haskell: Colorize repl prompt and output prefix

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -4,11 +4,19 @@ import Jambda.Evaluator
 import Jambda.Reader
 import Jambda.Types
 import qualified System.Console.Haskeline as Hline
+import qualified System.Console.ANSI.Codes as Term
+import qualified System.Console.ANSI.Types as TermT
 import System.Posix.IO (stdInput)
 import System.Posix.Terminal (queryTerminal)
 
 jprint :: JForm -> Either String String
 jprint x = Right $ show x
+
+colorizeStr :: TermT.Color -> String -> String
+colorizeStr color str =
+  Term.setSGRCode [TermT.SetColor TermT.Foreground TermT.Dull color]
+    ++ str
+    ++ Term.setSGRCode [Term.Reset]
 
 rep :: String -> Either String String
 rep x = Right x >>= jread >>= jeval >>= jprint
@@ -19,13 +27,13 @@ repl =
   where
     loop :: Hline.InputT IO ()
     loop = do
-      minput <- Hline.getInputLine "λ "
+      minput <- Hline.getInputLine (colorizeStr TermT.Blue "λ ")
       case minput of
         Nothing -> return ()
         Just input -> do
           case rep input of
-            Left out -> Hline.outputStrLn $ "↯ " ++ out
-            Right out -> Hline.outputStrLn $ "∎ " ++ out
+            Left out -> Hline.outputStrLn $ (colorizeStr TermT.Red "↯ ") ++ out
+            Right out -> Hline.outputStrLn $ (colorizeStr TermT.Green "∎ ") ++ out
           loop
 
 main :: IO ()
