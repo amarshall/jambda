a5a37298aa6867403b3bbbec7938069ded272364
Andrew Marshall <andrew@johnandrewmarshall.com>
2018-09-11 22:37:02 -0400

    haskell: Improve core/str
    À la Ruby `to_s` vs. `inspect` (this is the former).

--- haskell/src/Jambda/Core.hs
+++ haskell/src/Jambda/Core.hs
@@ -18,7 +18,17 @@ jLength [JString str] = Right $ JInteger $ (length str)
 jLength _ = Left "TypeError"
 
 jStr :: [JForm] -> JResult
-jStr (form:[]) = return $ JString (show form)
+jStr (form:[]) =
+  toStr form >>= (return . JString)
+  where
+    toStr (JBoolean False) = Right "false"
+    toStr (JBoolean True) = Right "true"
+    toStr (JFloat x) = Right $ show x
+    toStr (JInteger x) = Right $ show x
+    toStr (JList x) = Right $ show x
+    toStr (JString x) = Right x
+    toStr JNothing = Right ""
+    toStr _ = Left "TypeError: cannot convert to string"
 jStr (_:_) = Left "wrong arity"
 jStr [] = return $ JString ""
 
